9c688bce980b7e3d17f18bccd0a67c9a1bd6699a
daviduvalle <david.uvalle@gmail.com>
2020-04-23 22:06:24 -0700

    Added index from cluster to list of docs

--- clustering.py
+++ clustering.py
@@ -42,7 +42,7 @@ class Clustering:
         ax.set_title('Sores by clusters')
         plt.show()
 
-    def kmeans(self, num_keywords):
+    def kmeans(self):
         ''' Runs k-means and prints the top keywords per cluster
         :param num_keywords: number of top keywords to print
         :return centroids, labels, tf-idf features
@@ -52,33 +52,40 @@ class Clustering:
         cluster_labels = km.fit_predict(self.matrix)
         sorted_centroids = km.cluster_centers_.argsort()[:, ::-1]
 
-        for i in range(CLUSTER_NUMBER):
-            print('Top words by cluster {}'.format(i))
-            for index in sorted_centroids[i, :num_keywords]:
-                print(' %s' % feature_names[index], end='')
-            print()
-
         return sorted_centroids, cluster_labels, feature_names
 
-    def plot(self, labels, feature_names):
+    def plot(self, labels, html_path):
         dense = self.matrix.todense()
         pca = PCA(n_components=3).fit_transform(dense)
         norm = plt.Normalize(np.min(labels), np.max(labels))
         normalized = norm(labels)
         fig = px.scatter_3d(x=pca[:,0], y=pca[:,1], z=pca[:,2], color=normalized)
-        fig.show()
+        fig.write_html(html_path)
+        print('Clustering visualization using k-means saved as: {}'.format(html_path))
 
-        '''
-        norm = plt.Normalize(np.min(labels), np.max(labels))
-        map = cm.ScalarMappable(norm=norm, cmap=cm.hot)
-        output = map.to_rgba(labels)
-        plt.scatter(pca[:,0], pca[:,1], c=output)
-        plt.show()
-        '''
+    def save_file(self, centroids, labels, feature_names, file_path):
+        print('Top 10 words by cluster')
+        cluster_to_doc = {}
+        for i in range(CLUSTER_NUMBER):
+            print('cluster #:{}'.format(i))
+            cluster_keywords = ''
+            for index in centroids[i, :10]:
+                cluster_keywords += feature_names[index] + ' '
+            print(cluster_keywords)
+            print('labels {}'.format(labels.size))
+            cluster_to_doc[i] = list()
+            for e in range(0, labels.size):
+                if labels[e] == i:
+                    cluster_to_doc[i].append(self.ids[e])
+
+        print('Docs by cluster')
+        for i in range(CLUSTER_NUMBER):
+            print('Cluster {} docs {}'.format(i, len(cluster_to_doc[i])))
 
 
 if __name__ == '__main__':
     clustering = Clustering()
-    centroids, labels, feature_names = clustering.kmeans(3)
-    clustering.plot(labels, feature_names)
+    centroids, labels, feature_names = clustering.kmeans()
+    clustering.plot(labels, 'output.html')
+    clustering.save_file(centroids, labels, feature_names, 'clusters.json')
 
